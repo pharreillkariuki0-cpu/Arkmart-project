25fa0c35365979c11a5974ec611bcd1e18343633
pharreillkariuki0-cpu <pharreillkariuki0@gmail.com>
2025-12-09 20:05:53 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
  
 function Kariuki(){
-  let cars = ["Bmw", "Mazda", "Subaru", "Toyota", "Porshe"];
+  let cars = ["Bmw", "Mazda", "Subaru", "Toyota", "Porshe", "Ford", "Fortuner","Jeep","Harrier","Lexus"];
+  let uni = ["Karatina University","Maseno", "JKUT", "Egerton", "Moi", "UON", "KU", "MKU", "Kirinyaga", "Dedan Kimathi"]
+  let tribes = ["Kalenjin", "Luhya", "Mijikenda","Luo", "Masai","Akamba", "Kikuyu","Ameru","Aembu","Pokot"];
  //return <p>Hello World!</p>
  return(
   <>
+  <h3>Cars</h3>
     <ul>
-      {cars.map((car) => (<li>{car}</li>) )}
+      {cars.map((car)=>(<li>{car}</li>))}
     </ul>
+    <h3>Universities</h3>
+  <ul>
+    {uni.map((unis) => (<li>{unis}</li>))}
+  </ul>
+  <h3>Tribes</h3>
+  <ul>
+    {tribes.map((tribe) => (<li>{tribe}</li>))}
+  </ul>
   </>
  )
  
